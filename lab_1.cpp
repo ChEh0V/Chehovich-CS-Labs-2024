e9f6cd85fd347bf8b2f4d6ad1e2511e400397a28
ur0ch <Yura042006@mail.ru>
2024-09-12 14:55:46 +0300

    guck

--- lab_1.cpp
+++ lab_1.cpp
@@ -1,1 +1,11 @@
 #include <iostream>
+int main(int, char**){
+    int chas{};
+    int minut{};
+    char time[100];
+    std:: cin >> chas >> minut;
+    if (chas > 12 || minut > 60 || chas < 0 || minut < 0){
+        std::cout << "в вводе ошибка";
+    }
+    return 0;
+}
