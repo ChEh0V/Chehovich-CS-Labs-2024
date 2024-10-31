b46c2c7af75cb027d16812fee8af79a0cea50f60
ur0ch <Yura042006@mail.ru>
2024-10-31 20:22:13 +0300

    last version

--- NonLinearEquations.hpp
+++ NonLinearEquations.hpp
@@ -1,16 +1,27 @@
-#include <iostream>
-namespace interface{
+#pragma once
 
-enum class Method : int{Iterative = 1,Newton, HalfDivision};
+namespace NonLinearEquations {
+enum class EquationMethod : int {
+    Iterative = 1,
+    Newton,
+    HalfDivision,
+};
 
-double SearchRootIterativeMethod(double ratio, double accuracy, int& countIterations);
-double SearchRootNewtonMethod(double ratio, double accuracy, int& countIterations);
-double SearchRootHalfDivisionMethod(double ratio, double accuracy, int& countIterations, double leftPointer, double rightPointer);
-
-void IterationMethod();
-void NewtonMethod();
-void MethodHalfDivision();
-
+struct rootCalculationResult {
+    double root = 0.;
+    int countIterations = 0;
+    bool solved = true;
+};
 void ChooseSolution();
 void TaskStart();
-}
+
+[[nodiscard]] rootCalculationResult SearchRootIterativeMethod(double ratio, double accuracy);
+void IterationMethod();
+
+[[nodiscard]] rootCalculationResult SearchRootNewtonMethod(double ratio, double accuracy);
+void NewtonMethod();
+
+[[nodiscard]] rootCalculationResult SearchRootHalfDivisionMethod(double ratio, double accuracy, double leftPointer, double rightPointer);
+void MethodHalfDivision();
+
+}  // namespace NonLinearEquations
